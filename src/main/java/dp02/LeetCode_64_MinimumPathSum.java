e8004914082c3426502a63f1c30d31356cdc764a
l05 <285296372@qq.com>
2018-07-05 18:43:37 +0800

    64

--- src/main/java/dp02/LeetCode_64_MinimumPathSum.java
+++ src/main/java/dp02/LeetCode_64_MinimumPathSum.java
@@ -7,14 +7,16 @@ package dp02;
 public class LeetCode_64_MinimumPathSum {
     public static void main(String[] args) {
 //        [4,6,4,7,7,4,8,2,1],[1,9,6,9,8,2,9,7,2],[5,5,7,5,8,7,9,1,4],[0,7,9,9,1,5,3,9,4]
-        int grid[][] = {{7,4,8},
-                        {1,8,2}};
+        int grid[][] = {{7,4,3},
+                        {1,8,3}};
         System.out.println("minPathSum_dp_otherWay_loop");
         System.out.println(minPathSum_dp_otherWay_loop(grid));
         System.out.println("minPathSum_dp_loop");
         System.out.println(minPathSum_dp_loop(grid));
         System.out.println("minPathSum_dp_loop_lessMemory");
         System.out.println(minPathSum_dp_loop_lessMemory(grid));
+        System.out.println("minPathSum_dp_loop_bestMemory");
+        System.out.println(minPathSum_dp_loop_bestMemory(grid));
     }
     /**
      * 状态转换方程:
@@ -63,19 +65,35 @@ public class LeetCode_64_MinimumPathSum {
      */
     public static int minPathSum_dp_loop_bestMemory(int[][] grid) {
         int m = grid.length, n = grid[0].length;
-        int dp[] = new int[n];
+        if(m > n) {
+            int dp[] = new int[n];
 
-        for (int i = 1; i < n; ++i) dp[i] = dp[i-1] + grid[0][i-1];
-        for (int i = 1; i < m; ++i) {
-            /**
-             * 单独解决dp[0]累加更新问题
-             */
-            dp[0] += grid[i-1][0];
-            for (int j = 1; j < n; ++j) {
-                dp[j] = Math.min(dp[j] + grid[i-1][j], dp[j-1] + grid[i][j-1]);
+            for (int i = 1; i < n; ++i) dp[i] = dp[i-1] + grid[0][i-1];
+            for (int i = 1; i < m; ++i) {
+                /**
+                 * 单独解决dp[0]累加更新问题
+                 */
+                dp[0] += grid[i - 1][0];
+                for (int j = 1; j < n; ++j) {
+                    dp[j] = Math.min(dp[j] + grid[i-1][j], dp[j-1] + grid[i][j-1]);
+                }
             }
+            return dp[n - 1] + grid[m - 1][n - 1];
+        }else{
+            int dp[] = new int[m];
+
+            for (int i = 1; i < m; ++i) dp[i] = dp[i-1] + grid[i-1][0];
+            for (int i = 1; i < n; ++i) {
+                /**
+                 * 单独解决dp[0]累加更新问题
+                 */
+                dp[0] += grid[0][i-1];
+                for (int j = 1; j < m; ++j) {
+                    dp[j] = Math.min(dp[j] + grid[j][i - 1], dp[j - 1] + grid[j-1][i]);
+                }
+            }
+            return dp[m - 1] + grid[m - 1][n - 1];
         }
-        return dp[n-1] + grid[m-1][n-1];
     }
 
     /**
