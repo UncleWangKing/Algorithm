e1a9abbd3f355861d362d494b02d124462a574c4
l05 <285296372@qq.com>
2018-07-05 18:51:56 +0800

    64

--- src/main/java/dp02/LeetCode_64_MinimumPathSum.java
+++ src/main/java/dp02/LeetCode_64_MinimumPathSum.java
@@ -81,7 +81,7 @@ public class LeetCode_64_MinimumPathSum {
                     dp[j] = Math.min(dp[j] + grid[i-1][j], dp[j-1] + grid[i][j-1]);
                 }
             }
-            return dp[n - 1] + grid[m - 1][n - 1];
+            return dp[n-1] + grid[m-1][n-1];
         }else{
             int dp[] = new int[m];
 
@@ -92,10 +92,10 @@ public class LeetCode_64_MinimumPathSum {
                  */
                 dp[0] += grid[0][i-1];
                 for (int j = 1; j < m; ++j) {
-                    dp[j] = Math.min(dp[j] + grid[j][i - 1], dp[j - 1] + grid[j-1][i]);
+                    dp[j] = Math.min(dp[j] + grid[j][i-1], dp[j-1] + grid[j-1][i]);
                 }
             }
-            return dp[m - 1] + grid[m - 1][n - 1];
+            return dp[m-1] + grid[m-1][n-1];
         }
     }
 
@@ -108,14 +108,14 @@ public class LeetCode_64_MinimumPathSum {
         int dp[][] = new int[m][n];
 
         dp[0][0] = grid[0][0];
-        for (int i = 1; i < m; ++i) dp[i][0] = grid[i][0] + dp[i - 1][0];
-        for (int i = 1; i < n; ++i) dp[0][i] = grid[0][i] + dp[0][i - 1];
+        for (int i = 1; i < m; ++i) dp[i][0] = grid[i][0] + dp[i-1][0];
+        for (int i = 1; i < n; ++i) dp[0][i] = grid[0][i] + dp[0][i-1];
 
         for (int i = 1; i < m; ++i) {
             for (int j = 1; j < n; ++j) {
-                dp[i][j] = grid[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1]);
+                dp[i][j] = grid[i][j] + Math.min(dp[i-1][j], dp[i][j-1]);
             }
         }
-        return dp[m - 1][n - 1];
+        return dp[m-1][n-1];
     }
 }
