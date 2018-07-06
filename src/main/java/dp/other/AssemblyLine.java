981ddb5ff9a384d3f20af6315cc0dc279eb3bca4
l05 <285296372@qq.com>
2018-07-06 18:36:22 +0800

    assemblyLine

--- src/main/java/dp/other/AssemblyLine.java
+++ src/main/java/dp/other/AssemblyLine.java
@@ -14,36 +14,27 @@ public class AssemblyLine {
 	 * 1.去掉进入和离开生产线时候的消耗---本身也是加在生产线的头尾节点，没有特别意义
 	 * 2.去掉生产线的数量参数---可直接通过数组长度获得
 	 * 状态转换方程
-	 * if(1 == j)
-	 *  f1[j] = e1 + a1,1
-	 *  f2[j] = e2 + a2,1
+	 * if(1 == i)
+	 *  dp[0][0] = line[0][0]
+	 *  dp[1][0] = line[1][0]
 	 * else
-	 * 	f1[j] = min(f1[j−1] + a1,j ,f2[j−1] +t2,j−1 + a1,j ) （j=2,3…n）
-	 *  f2[j] = min(f2[j−1] + a2,j ,f1[j−1] +t1,j−1 + a2,j ) （j=2,3…n）
+	 *  dp[0][i] = Math.min(dp[1][i-1] + line[0][i] + trans[1][i-1], dp[0][i-1] + line[0][i]);
+	 *  dp[1][i] = Math.min(dp[0][i-1] + line[1][i] + trans[0][i-1], dp[1][i-1] + line[1][i]);
 	 */
 	
 	public static int fastestWay(int[][] line,int[][] trans) {
+		int m = line.length;
 		int n = line[0].length;
-		int []dp = new int [n];
-		int lineNumber = 0;
-		if(line[0][0] + trans[0][0] < line[1][0] + trans[1][0]){
-			lineNumber = 0;
-			dp[0] = line[0][0];
-		}
-		else {
-			lineNumber = 1;
-			dp[0] = line[1][0];
-		}
+		int [][]dp = new int [m][n];
+		dp[0][0] = line[0][0];
+		dp[1][0] = line[1][0];
 
 		for (int i = 1; i < n; i++) {
-			if(0 == lineNumber){
-				//左换线 右直走
-				dp[i] = Math.min(dp[i-1] + line[1][i] + trans[0][i-1],dp[i-1] + line[0][i]);
-			}else{
-				dp[i] = Math.min(dp[i-1] + line[0][i] + trans[1][i-1],dp[i-1] + line[1][i]);
-			}
+			//左换线 右直走
+			dp[0][i] = Math.min(dp[1][i-1] + line[0][i] + trans[1][i-1], dp[0][i-1] + line[0][i]);
+			dp[1][i] = Math.min(dp[0][i-1] + line[1][i] + trans[0][i-1], dp[1][i-1] + line[1][i]);
 		}
 
-		return dp[n-1];
+		return Math.min(dp[0][n-1], dp[1][n-1]);
 	}
 }
