78f59451f996712ce1f6ba6381781b12cde7159e
UncleWangKing <285296372@qq.com>
2018-07-08 20:07:19 +0800

    63

--- src/main/java/dp/dp02/LeetCode_63_UniquePathsII.java
+++ src/main/java/dp/dp02/LeetCode_63_UniquePathsII.java
@@ -48,19 +48,14 @@ public class LeetCode_63_UniquePathsII {
     public static int uniquePathsWithObstacles_dp_loop(int[][] obstacleGrid) {
         int m = obstacleGrid.length, n = obstacleGrid[0].length;
         int [][]dp = new int[m][n];
+        if(1 == obstacleGrid[0][0])
+            return 0;
+        dp[0][0] = 1;
+        for (int i = 1; i < m; ++i)
+            dp[i][0] = 1 == obstacleGrid[i][0] ? 0 : dp[i-1][0];
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if(1 == obstacleGrid[i][j])
-                    dp[i][j] = 0;
-                else if(0 == i && 0 == j)
-                    dp[i][j] = 1;
-                else if(0 == i && 0 < j)
-                    dp[i][j] = dp[i][j-1];
-                else if(0 < i && 0 == j)
-                    dp[i][j] = dp[i-1][j];
-            }
-        }
+        for (int i = 1; i < n; ++i)
+            dp[0][i] = 1 == obstacleGrid[0][i] ? 0 : dp[0][i-1];
 
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
