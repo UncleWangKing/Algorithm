7f7ce2fff3fe96259d9af511e0565f236a24f5fe
UncleWangKing <285296372@qq.com>
2018-07-08 19:32:14 +0800

    64

--- src/main/java/dp/dp02/LeetCode_64_MinimumPathSum.java
+++ src/main/java/dp/dp02/LeetCode_64_MinimumPathSum.java
@@ -114,8 +114,8 @@ public class LeetCode_64_MinimumPathSum {
         int dp[][] = new int[m][n];
 
         dp[0][0] = grid[0][0];
-        for (int i = 1; i < m; ++i) dp[i][0] = grid[i][0] + dp[i-1][0];
-        for (int i = 1; i < n; ++i) dp[0][i] = grid[0][i] + dp[0][i-1];
+        for (int i = 1; i < m; ++i) dp[i][0] = dp[i-1][0] + grid[i][0];
+        for (int i = 1; i < n; ++i) dp[0][i] = dp[0][i-1] + grid[0][i];
 
         for (int i = 1; i < m; ++i) {
             for (int j = 1; j < n; ++j) {
