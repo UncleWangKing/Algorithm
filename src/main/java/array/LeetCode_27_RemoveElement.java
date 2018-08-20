937bf7fca1221090705a43b74b90846e5ccb0c9e
UncleWangKing <285296372@qq.com>
2018-08-21 00:22:53 +0800

    27

--- src/main/java/array/LeetCode_27_RemoveElement.java
+++ src/main/java/array/LeetCode_27_RemoveElement.java
@@ -10,9 +10,9 @@ public class LeetCode_27_RemoveElement {
     }
 
     /**
-     * 双指针就是最优思路了 如何写优雅 这是抄的100%写法
+     * 双指针 你看看人家！！！
      */
-    public static int removeElement(int[] nums, int val) {
+    public static int removeElement2(int[] nums, int val) {
         if(1 > nums.length)
             return 0;
         int result = 0, left = 0, right = nums.length - 1;
@@ -26,6 +26,39 @@ public class LeetCode_27_RemoveElement {
                 right--;
             }
         }
+        return result;
+    }
+
+    /**
+     * 双指针 丑陋的自己的写法
+     */
+    public static int removeElement(int[] nums, int val) {
+        if(1 > nums.length)
+            return 0;
+        if(1 == nums.length)
+            return val == nums[0] ? 0 : 1;
+        int left = 0;
+        int right = nums.length - 1;
+        int result = 0;
+        while(left < right){
+            if(nums[left] == val){
+                while(left < right){
+                    if(nums[right] != val){
+                        result++;
+                        nums[left] = nums[right--];
+                        break;
+                    }else
+                    right--;
+                }
+            }else
+                result++;
+            left++;
+            if(left == right){
+                if(nums[left] != val)
+                    result++;
+            }
+        }
+
         return result;
     }
 }
