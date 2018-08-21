636fe8275d3f43a3a85a8cdfa24a8475802a55f7
UncleWangKing <285296372@qq.com>
2018-08-22 00:07:03 +0800

    真香

---
+++ src/main/java/array/LeetCode_40_CombinationSumII.java
@@ -0,0 +1,39 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LeetCode_40_CombinationSumII {
+    public static void main(String[] args) {
+        int list[] = {10,1,2,7,6,1,5};
+        System.out.println(combinationSum2(list, 8));
+    }
+
+    public static List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList();
+        Arrays.sort(candidates);
+        backtracking(candidates, 0, result, target, new ArrayList());
+        return result;
+    }
+
+    private static void backtracking(int[] candidates,
+                                     int step,
+                                     List<List<Integer>> result,
+                                     int target,
+                                     List<Integer> curr) {
+        if (target > 0) {
+            for (int i = step; i < candidates.length; i++) {
+                //只去一个 去重关键
+                if (candidates[i] > target || ((i > step) && (candidates[i - 1] == candidates[i])))
+                    continue;
+
+                curr.add(candidates[i]);
+                backtracking(candidates, i + 1, result, target - candidates[i], curr);
+                curr.remove(curr.size() - 1);//回溯操作
+            }
+        } else if (target == 0) {
+            result.add(new ArrayList(curr));
+        }
+    }
+}
