ee1f51f485ba9cf6b964c874bfdfb3f643ecfd52
UncleWangKing <285296372@qq.com>
2018-08-24 18:56:26 +0800

    57

--- src/main/java/array/LeetCode_57_InsertInterval.java
+++ src/main/java/array/LeetCode_57_InsertInterval.java
@@ -1,31 +1,34 @@
 package array;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
 
-public class LeetCode_56_MergeIntervals {
-
+public class LeetCode_57_InsertInterval {
     private static class Interval {
-         int start;
-         int end;
-         Interval() { start = 0; end = 0; }
-         Interval(int s, int e) { start = s; end = e; }
-         public String toString(){
-             return "[" + start + ","+end + "]";
-         }
-     }
-
-    public static void main(String[] args) {
-//        [[2,3],[4,5],[6,7],[8,9],[1,10]]
-        List<Interval> list = new LinkedList<>();
-        list.add(new Interval(2,3));
-        list.add(new Interval(4,5));
-        list.add(new Interval(6,7));
-        list.add(new Interval(8,9));
-        list.add(new Interval(1,10));
-        System.out.println(merge(list));
+        int start;
+        int end;
+        Interval() { start = 0; end = 0; }
+        Interval(int s, int e) { start = s; end = e; }
+        public String toString(){
+            return "[" + start + ","+end + "]";
+        }
     }
 
-    public static List<Interval> merge(List<Interval> intervals) {
+    public static void main(String[] args) {
+//        [1,2],[3,5],[6,7],[8,10],[12,16]
+        List<Interval> list = new LinkedList<>();
+        list.add(new Interval(1,2));
+        list.add(new Interval(3,5));
+        list.add(new Interval(6,7));
+        list.add(new Interval(8,10));
+        list.add(new Interval(12,16));
+        System.out.println(insert(list, new Interval(4, 8)));
+    }
+
+    public static List<Interval> insert(List<Interval> intervals, Interval newInterval) {
+        intervals.add(newInterval);
         List<Interval> res = new LinkedList<>();
         Collections.sort(intervals, new Comparator<Interval>() {
             @Override
