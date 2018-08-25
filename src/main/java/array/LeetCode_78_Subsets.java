346bb3d8afdfea5b65829b3f9d53557fbaefb894
UncleWangKing <285296372@qq.com>
2018-08-26 01:16:45 +0800

    78

---
+++ src/main/java/array/LeetCode_78_Subsets.java
@@ -0,0 +1,39 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeetCode_78_Subsets {
+    public static void main(String[] args) {
+        int list[] = {1,2,3};
+        System.out.println(subsets2(list));
+    }
+    public static List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        dfs(res, temp, nums, 0);
+        return res;
+    }
+    private static void dfs(List<List<Integer>> res, List<Integer> temp, int[] nums, int j) {
+        res.add(new ArrayList<>(temp));
+        for(int i = j; i < nums.length; i++) {
+            temp.add(nums[i]);  //① 加入 nums[i]
+            dfs(res, temp, nums, i + 1);  //② 递归
+            temp.remove(temp.size() - 1);  //③ 移除 nums[i]
+        }
+    }
+    public static List<List<Integer>> subsets2(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        res.add(new ArrayList<>());
+        for (int num : nums) {  // ①从数组中取出每个元素
+            int size = res.size();
+            for (int i = 0; i < size; i++) {
+                List<Integer> temp = new ArrayList<>(res.get(i));  // ②逐一取出中间结果集
+                temp.add(num);  // ③将 num 放入中间结果集
+                res.add(temp);  // ④加入到结果集中
+            }
+        }
+        return res;
+    }
+
+}
