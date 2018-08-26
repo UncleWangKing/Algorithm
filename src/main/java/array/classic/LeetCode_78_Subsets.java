88f7d43a621e445e23845388aa2a35cc2620e66c
UncleWangKing <285296372@qq.com>
2018-08-26 15:18:36 +0800

    78

--- src/main/java/array/classic/LeetCode_78_Subsets.java
+++ src/main/java/array/classic/LeetCode_78_Subsets.java
@@ -6,23 +6,13 @@ import java.util.List;
 public class LeetCode_78_Subsets {
     public static void main(String[] args) {
         int list[] = {1,2,3};
-        System.out.println(subsets(list));
+        System.out.println(subsets3(list));
     }
+
+    /**
+     * 深搜的循环版
+     */
     public static List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        List<Integer> temp = new ArrayList<>();
-        dfs(res, temp, nums, 0);
-        return res;
-    }
-    private static void dfs(List<List<Integer>> res, List<Integer> temp, int[] nums, int j) {
-        res.add(new ArrayList<>(temp));
-        for(int i = j; i < nums.length; i++) {
-            temp.add(nums[i]);  //① 加入 nums[i]
-            dfs(res, temp, nums, i + 1);  //② 递归
-            temp.remove(temp.size() - 1);  //③ 移除 nums[i]
-        }
-    }
-    public static List<List<Integer>> subsets2(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         res.add(new ArrayList<>());
         for (int num : nums) {  // ①从数组中取出每个元素
@@ -36,4 +26,45 @@ public class LeetCode_78_Subsets {
         return res;
     }
 
+    /**
+     * 最难理解的一种 将每个位置的值是否放入转化成二叉树 深搜
+     */
+    public static List<List<Integer>> subsets2(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        dfs(res, temp, nums, 0);
+        return res;
+    }
+
+    private static void dfs(List<List<Integer>> res, List<Integer> temp, int[] nums, int j) {
+        res.add(new ArrayList<>(temp));
+        for(int i = j; i < nums.length; i++) {
+            temp.add(nums[i]);  //① 加入 nums[i]
+            dfs(res, temp, nums, i + 1);  //② 递归
+            temp.remove(temp.size() - 1);  //③ 移除 nums[i]
+        }
+    }
+
+    /**
+     * 利用二进位来代表对应下标是否被加入 -- 妙不可言
+     */
+    public static List<List<Integer>> subsets3(int[] nums) {
+        int len = nums.length;
+        List<List<Integer>> res = new ArrayList<>();
+        res.add(new ArrayList<>());//开始加入一个空集
+
+        long bit = 1, bitmax = (1 << len);
+        while(bit < bitmax) {
+            List<Integer> tmpres = new ArrayList<>();
+            long curBit = bit;
+            for(int i = 0; i < len; i++){//依次检测前len个二进制位
+                if(1 == (curBit & 1))
+                    tmpres.add(nums[i]);
+                curBit >>= 1;
+            }
+            res.add(tmpres);
+            bit++;
+        }
+        return res;
+    }
 }
