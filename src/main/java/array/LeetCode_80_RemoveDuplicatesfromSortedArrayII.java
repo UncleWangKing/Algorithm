610188a70e211b4addd4fa26d60428c37214d6f9
UncleWangKing <285296372@qq.com>
2018-08-29 09:21:18 +0800

    80

--- src/main/java/array/LeetCode_80_RemoveDuplicatesfromSortedArrayII.java
+++ src/main/java/array/LeetCode_80_RemoveDuplicatesfromSortedArrayII.java
@@ -4,11 +4,33 @@ import util.ZDaPangArrayUtil;
 
 public class LeetCode_80_RemoveDuplicatesfromSortedArrayII {
     public static void main(String[] args) {
-        int []list = {0,0,1,1,1,2,2,3,3,4};
+        int []list = {0,0,1,1,1,1,2,3,3};
         System.out.println(removeDuplicates(list));
         ZDaPangArrayUtil.printArray(list);
     }
     public static int removeDuplicates(int[] nums) {
-        return 0;
+        int pos = 0;
+        int flag = 0;
+        int last = 0;
+        for(int i = 0;i < nums.length;i++){
+            if(i == 0){
+                last = nums[i];
+                flag = 1;
+                pos++;
+            }
+            else{
+                if(nums[i] == last){
+                    flag++;
+                }
+                else{
+                    flag = 1;
+                }
+                if(flag <= 2){
+                    nums[pos] = nums[i];pos++;
+                }
+                last = nums[i];
+            }
+        }
+        return pos;
     }
 }
