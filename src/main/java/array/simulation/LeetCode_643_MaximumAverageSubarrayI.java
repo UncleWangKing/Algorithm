3d8662c7cf791da2f618f6bc976a0988e40a70e1
l05 <285296372@qq.com>
2018-09-18 18:58:10 +0800

    643 又是水

---
+++ src/main/java/array/simulation/LeetCode_643_MaximumAverageSubarrayI.java
@@ -0,0 +1,23 @@
+package array.simulation;
+
+/**
+ * @author ZhangDaPang 285296372@qq.com
+ * @date 2018/9/18 18:38
+ */
+public class LeetCode_643_MaximumAverageSubarrayI {
+    public static void main(String[] args) {
+        int list[] = {1,12,-5,-6,50,3};
+        System.out.println(findMaxAverage(list, 4));
+    }
+
+    public static double findMaxAverage(int[] nums, int k) {
+        double temp = k;
+
+        double local = 0;
+        double global = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+        }
+    }
+}
