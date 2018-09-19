bcd236e694182e6f385dac4e895376feef47160e
l05 <285296372@qq.com>
2018-09-19 11:22:51 +0800

    643 又是水

--- src/main/java/array/simulation/LeetCode_643_MaximumAverageSubarrayI.java
+++ src/main/java/array/simulation/LeetCode_643_MaximumAverageSubarrayI.java
@@ -11,13 +11,17 @@ public class LeetCode_643_MaximumAverageSubarrayI {
     }
 
     public static double findMaxAverage(int[] nums, int k) {
-        double temp = k;
-
         double local = 0;
-        double global = 0;
+        double global = Integer.MIN_VALUE;
 
         for (int i = 0; i < nums.length; i++) {
-
+            if(i >= k){
+                global = Math.max(global, local);
+                local -= nums[i - k];
+            }
+            local += nums[i];
         }
+
+        return Math.max(global, local) / (double)k;//解决最后一次未统计的local
     }
 }
