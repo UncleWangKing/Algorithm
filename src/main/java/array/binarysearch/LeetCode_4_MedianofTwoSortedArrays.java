c1023b772b0447751b789dbff01572d1b3b32def
UncleWangKing <285296372@qq.com>
2018-09-20 00:58:12 +0800

    4

--- src/main/java/array/binarysearch/LeetCode_4_MedianofTwoSortedArrays.java
+++ src/main/java/array/binarysearch/LeetCode_4_MedianofTwoSortedArrays.java
@@ -25,8 +25,8 @@ public class LeetCode_4_MedianofTwoSortedArrays {
     //log(m + n)
     public static double findMedianSortedArrays2(int[] nums1, int[] nums2) {
         int m = nums1.length, n = nums2.length;
-        if (m < n) return findMedianSortedArrays(nums2, nums1);
-        if (n == 0) return (nums1[(m - 1) / 2] + nums1[m / 2]) / 2.0;
+        if (m < n) return findMedianSortedArrays(nums2, nums1);//确保数组1长度大于数组2 -- m >= n
+        if (n == 0) return (nums1[(m - 1) / 2] + nums1[m / 2]) / 2.0;//如果数组2是空 -- 剪枝
         int left = 0, right = 2 * n;
         while (left <= right) {
             int mid2 = (left + right) / 2;
@@ -44,31 +44,27 @@ public class LeetCode_4_MedianofTwoSortedArrays {
     //log(m + n) -- best
     public static double findMedianSortedArrays3(int[] nums1, int[] nums2) {
         int total = nums1.length + nums2.length;
-        if (total % 2 == 1) {
+        if (total % 2 == 1)
             return findKth(nums1, 0, nums2, 0, total / 2 + 1);
-        }else {
+        else
             return (findKth(nums1, 0, nums2, 0, total / 2) + findKth(nums1, 0, nums2, 0, total / 2 + 1)) / 2.0;
-        }
     }
 
     public static int findKth(int[] nums1, int a, int[] nums2, int b, int k) {
-        if (nums1.length - a > nums2.length - b) {
+        if (nums1.length - a > nums2.length - b)
             return findKth(nums2, b, nums1, a, k);
-        }
-        if (nums1.length - a == 0) {
+        if (nums1.length - a == 0)
             return nums2[b + k - 1];
-        }
-        if (k == 1) {
+        if (k == 1)
             return Math.min(nums1[a], nums2[b]);
-        }
+
         int k1 = Math.min(nums1.length - a, k / 2);
         int k2 = k - k1;
-        if (nums1[a + k1 - 1] < nums2[b + k2 - 1]) {
+        if (nums1[a + k1 - 1] < nums2[b + k2 - 1])
             return findKth(nums1, a + k1, nums2, b, k - k1);
-        }else if (nums1[a + k1 - 1] > nums2[b + k2 - 1]) {
+        else if (nums1[a + k1 - 1] > nums2[b + k2 - 1])
             return findKth(nums1, a, nums2, b + k2, k - k2);
-        }else {
+        else
             return nums1[a + k1 - 1];
-        }
     }
 }
