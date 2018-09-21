1bbdd8ae4d764afd09e1962dc2b6ed30011e774c
l05 <285296372@qq.com>
2018-09-21 15:33:12 +0800

    560

--- src/main/java/array/hashmap/LeetCode_560_SubarraySumEqualsK.java
+++ src/main/java/array/hashmap/LeetCode_560_SubarraySumEqualsK.java
@@ -13,6 +13,7 @@ public class LeetCode_560_SubarraySumEqualsK {
         System.out.println(subarraySum2(list, 2));
     }
 
+    //暴力 构造所有情况
     public static int subarraySum(int[] nums, int k) {
         int result = 0;
 
@@ -29,16 +30,16 @@ public class LeetCode_560_SubarraySumEqualsK {
 
         return result;
     }
-
+    //map
     public static int subarraySum2(int[] nums, int k) {
         int sum = 0, count = 0;
         Map<Integer, Integer> map = new HashMap<>();
         map.put(0, 1);
         for(int i = 0; i < nums.length; i++){
             sum += nums[i];
-            if(map.containsKey(sum - k))
+            if(map.containsKey(sum - k))//妙不可言 以i为结尾的满足条件子数组的个数
                 count += map.get(sum - k);
-            map.put(sum, map.getOrDefault(sum, 0) + 1);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);//缓存0-i之和sum出现的次数
         }
         return count;
     }
