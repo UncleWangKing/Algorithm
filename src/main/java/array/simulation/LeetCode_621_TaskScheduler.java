867e14be7d50e6fa6485a9f7acccb72091ea5ac7
l05 <285296372@qq.com>
2018-09-21 18:15:45 +0800

    621

--- src/main/java/array/simulation/LeetCode_621_TaskScheduler.java
+++ src/main/java/array/simulation/LeetCode_621_TaskScheduler.java
@@ -1,5 +1,7 @@
 package array.simulation;
 
+import java.util.Arrays;
+
 /**
  * @author ZhangDaPang 285296372@qq.com
  * @date 2018/9/21 16:15
@@ -12,17 +14,27 @@ public class LeetCode_621_TaskScheduler {
 
     /**
      * 分析:
-     * 任务时间固定 只需分析等待时间
-     * 任务种类大于延迟n 则无需等待
-     * 任务种类小于等于n 等待t == n - k + 1 -- k 任务种类
-     *
-     * 注意善用多类任务 比如 A B 2C 2延迟 可以0等待
-     *
-     * 然后问题抽象成一个横轴是A-Z 纵轴是数量的直方图 可假设从高到低排好序
+     * AAAABBBEEFFGG 3为例
+     * A---A---A---A
+     * AB--AB--AB--A   (加入B)
+     * ABE-ABE-AB--A   (加入E)
+     * ABEFABE-ABF-A   (加入F，每次尽可能填满或者是均匀填充)
+     * ABEFABEGABFGA   (加入G)
+     * 再来看一个例子：
+     * ACCCEEE 2
+     * 我们发现任务C和E都出现了三次，那么我们就将CE看作一个整体，在中间加入一个位置即可：
+     * CE-CE-CE
+     * CEACE-CE   (加入A)
+     * 注意最后面那个idle不能省略，不然就不满足相同两个任务之间要隔2个时间点了。
      */
     public static int leastInterval(char[] tasks, int n) {
-        if(1 == n)
-            return tasks.length;
+        int cnt[] = new int[26];
+        for (char task : tasks)
+            ++cnt[task - 'A'];
 
+        Arrays.sort(cnt);
+        int i = 25, mx = cnt[25], len = tasks.length;
+        while (i >= 0 && cnt[i] == mx) --i;
+        return Math.max(len, (mx - 1) * (n + 1) + 25 - i);
     }
 }
