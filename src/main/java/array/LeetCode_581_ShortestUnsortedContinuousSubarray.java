bc4cf232a8a491efe2c9f7fca667b3f88cbcffac
UncleWangKing <285296372@qq.com>
2018-09-24 02:34:32 +0800

    581

--- src/main/java/array/LeetCode_581_ShortestUnsortedContinuousSubarray.java
+++ src/main/java/array/LeetCode_581_ShortestUnsortedContinuousSubarray.java
@@ -4,11 +4,11 @@ import java.util.Arrays;
 
 public class LeetCode_581_ShortestUnsortedContinuousSubarray {
     public static void main(String[] args) {
-//        int list[] = {2,6,4,8,10,9,15};//5
+        int list[] = {2,6,4,8,10,9,15};//5
 //        int list[] = {1,3,2,4,5};//2
 //        int list[] = {1,3,5,2,4};//4
-        int list[] = {2,1,5,3,4};//5
-        System.out.println(findUnsortedSubarray2(list));
+//        int list[] = {2,1,5,3,4};//5
+        System.out.println(findUnsortedSubarray(list));
     }
     //逆序数组在中间 好办 双指针往中间走 然后回走 求差值
     public static int findUnsortedSubarray(int[] nums) {
@@ -55,6 +55,13 @@ public class LeetCode_581_ShortestUnsortedContinuousSubarray {
         nums[right] = temp;
     }
 
+    /**
+     * 我们新一个跟原数组一摸一样的数组，然后排序。
+     * 从数组起始位置开始，两个数组相互比较，
+     * 当对应位置数字不同的时候停止，同理再从末尾开始，
+     * 对应位置上比较，也是遇到不同的数字时停止，
+     * 这样中间一段就是最短无序连续子数组了
+     */
     public static int findUnsortedSubarray3(int[] nums) {
         int n = nums.length, i = 0, j = n - 1;
         int[] t = nums.clone();
@@ -63,15 +70,15 @@ public class LeetCode_581_ShortestUnsortedContinuousSubarray {
         while (j > i && nums[j] == t[j]) --j;
         return j - i + 1;
     }
-
+    //精彩
     public static int findUnsortedSubarray4(int[] nums) {
         int n = nums.length, start = -1, end = -2;
-        int mn = nums[n - 1], mx = nums[0];
+        int min = nums[n - 1], max = nums[0];
         for (int i = 1; i < n; ++i) {
-            mx = Math.max(mx, nums[i]);
-            mn = Math.min(mn, nums[n - 1 - i]);
-            if (mx > nums[i]) end = i;
-            if (mn < nums[n - 1 - i]) start = n - 1 - i;
+            max = Math.max(max, nums[i]);
+            min = Math.min(min, nums[n - 1 - i]);
+            if (max > nums[i]) end = i;//左边有数更大
+            if (min < nums[n - 1 - i]) start = n - 1 - i;//右边有数更小
         }
         return end - start + 1;
     }
