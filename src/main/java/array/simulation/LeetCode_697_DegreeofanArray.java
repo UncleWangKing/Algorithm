394c7578d5880d724aed8cb2fec0dcefb69a58cd
l05 <285296372@qq.com>
2018-09-27 18:33:59 +0800

    697

---
+++ src/main/java/array/simulation/LeetCode_697_DegreeofanArray.java
@@ -0,0 +1,17 @@
+package array.simulation;
+
+/**
+ * @author ZhangDaPang 285296372@qq.com
+ * @date 2018/9/27 18:29
+ */
+public class LeetCode_697_DegreeofanArray {
+    public static void main(String[] args) {
+        int listp[] = {1, 2, 2, 3, 1};
+
+        System.out.println(findShortestSubArray(listp));
+    }
+
+    public static int findShortestSubArray(int[] nums) {
+
+    }
+}
