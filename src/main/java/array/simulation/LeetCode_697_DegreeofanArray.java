d62b404548c77812a61daf4ebdf17b89378a53e2
UncleWangKing <285296372@qq.com>
2018-09-28 03:33:50 +0800

    697

--- src/main/java/array/simulation/LeetCode_697_DegreeofanArray.java
+++ src/main/java/array/simulation/LeetCode_697_DegreeofanArray.java
@@ -1,5 +1,8 @@
 package array.simulation;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author ZhangDaPang 285296372@qq.com
  * @date 2018/9/27 18:29
@@ -12,6 +15,19 @@ public class LeetCode_697_DegreeofanArray {
     }
 
     public static int findShortestSubArray(int[] nums) {
-
+        int n = nums.length, res = Integer.MAX_VALUE, degree = 0;
+        Map<Integer, Integer> m = new HashMap<>();
+        Map<Integer, Integer> startIdx = new HashMap<>();
+        for (int i = 0; i < n; ++i) {
+            m.put(nums[i], m.getOrDefault(nums[i], 0) +1);
+            if (!startIdx.containsKey(nums[i])) startIdx.put(nums[i], i);
+            if (m.get(nums[i]) == degree) {
+                res = Math.min(res, i - startIdx.get(nums[i]) + 1);
+            } else if (m.get(nums[i]) > degree) {
+                res = i - startIdx.get(nums[i]) + 1;
+                degree = m.get(nums[i]);
+            }
+        }
+        return res;
     }
 }
