4395f4f168b9dd4260d1703c3f33152a9dfd290e
UncleWangKing <285296372@qq.com>
2018-10-03 04:43:59 +0800

    719

--- src/main/java/array/simulation/LeetCode_719_FindK_thSmallestPairDistance.java
+++ src/main/java/array/simulation/LeetCode_719_FindK_thSmallestPairDistance.java
@@ -1,5 +1,7 @@
 package array.simulation;
 
+import java.util.Arrays;
+
 public class LeetCode_719_FindK_thSmallestPairDistance {
     public static void main(String[] args) {
         int []list = {1,3,1};
@@ -7,8 +9,24 @@ public class LeetCode_719_FindK_thSmallestPairDistance {
     }
 
     /**
-     * 思路:直接一看 暴力解法 组合个数是O(n^2) 再加上计算时间 肯定会超时
+     * 直觉思路:直接一看 暴力解法 组合个数是O(n^2) 再加上计算时间 肯定会超时
+     * 解决计算时间可以累加数组降到O(n)
+     * 本质是求第k小的差
+     * 那么先排序
+     * 相邻2数只差为n - 1个 如果k小于等于n - 1 那么 k 一定是在某个相邻两数的差中
+     * 同理 如果k 大于n-1 小于 (n - 1) + (n - 2) 也就是3数只差
+     * 这样可以快速确定符合要求的差所处级别 大幅降低计算成本
+     * 然而败给了 三个重复数的情况~ 显然不合理
      */
     public static int smallestDistancePair(int[] nums, int k) {
+        //排序
+        Arrays.sort(nums);
+        //构造累加数组
+        int sum[] = new int[nums.length];
+        sum[0] = nums[0];
+        for (int i = 1; i < nums.length; i++)
+            sum[i] = nums[i] + sum[i - 1];
+        int step = 2;//k为相距多少的数
+
     }
 }
