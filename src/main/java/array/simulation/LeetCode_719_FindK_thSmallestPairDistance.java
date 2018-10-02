803bb9c7fa81d9c964079d0359b15f06461acb78
UncleWangKing <285296372@qq.com>
2018-10-03 04:41:13 +0800

    717

---
+++ src/main/java/array/simulation/LeetCode_719_FindK_thSmallestPairDistance.java
@@ -0,0 +1,14 @@
+package array.simulation;
+
+public class LeetCode_719_FindK_thSmallestPairDistance {
+    public static void main(String[] args) {
+        int []list = {1,3,1};
+        System.out.println(smallestDistancePair(list, 1));
+    }
+
+    /**
+     * 思路:直接一看 暴力解法 组合个数是O(n^2) 再加上计算时间 肯定会超时
+     */
+    public static int smallestDistancePair(int[] nums, int k) {
+    }
+}
