26c3619bcfcdc2e33330d960449a2cc77a9e7b18
UncleWangKing <285296372@qq.com>
2018-10-08 04:24:41 +0800

    768

---
+++ src/main/java/array/classic/LeetCode_768_MaxChunksToMakeSortedII.java
@@ -0,0 +1,73 @@
+package array.classic;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class LeetCode_768_MaxChunksToMakeSortedII {
+    public static void main(String[] args) {
+        int list[] = {2,1,3,4,4};//4
+        System.out.println(maxChunksToSorted3(list));
+    }
+
+    /**
+     * 利用块内值和相同
+     * 有序 块内n个元素 迭代过程中 会使sum1 sum2相等n次
+     * 无序 一个完整块sum1 sum2只会相等一次
+     */
+    public static int maxChunksToSorted(int[] arr) {
+        int res = 0, sum1 = 0, sum2 = 0;
+        int [] expect = arr;
+        Arrays.sort(expect);
+        for (int i = 0; i < arr.length; ++i) {
+            sum1 += arr[i];
+            sum2 += expect[i];
+            if (sum1 == sum2) ++res;
+        }
+        return res;
+    }
+
+    /**
+     *  当前位置出现过的最大值小于等于之后还未遍历到的最小值时
+     *  说明当前位置是一个独立块儿的最后一个元素
+     *  arr被就地取材放rightMin了 因为最后的循环不需要arr
+     */
+    public static int maxChunksToSorted2(int[] arr) {
+        int res = 1, n = arr.length;
+        int [] leftMax = arr.clone();
+        for (int i = 1; i < n; ++i) leftMax[i] = Math.max(arr[i], leftMax[i - 1]);
+        for (int i = n - 2; i >= 0; --i) arr[i] = Math.min(arr[i], arr[i + 1]);
+        for (int i = 0; i < n - 1; ++i)
+            if (leftMax[i] <= arr[i + 1]) ++res;
+
+        return res;
+    }
+
+    /**
+     * 单调栈
+     * 和解法2 类似
+     * 独立块儿中最右边的值 一定是块里最小的值
+     * 独立块儿中最左边的值 一定是块里最大的值
+     * 可随意记录最大值 最小值的数量
+     * 这里记录最大值数量  ---- grandyang博客这里没有看清这一层 算是超过了大佬当时的思维？
+     * 这样最大值的存在数量 也就是块儿的数量了
+     * [2,1,3,4,4] 为例
+     * 2
+     * 2
+     * 2 3
+     * 2 3 4
+     * 2 3 4 4
+     */
+    public static int maxChunksToSorted3(int[] arr) {
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < arr.length; ++i) {
+            if (st.isEmpty() || st.peek() <= arr[i]) {
+                st.push(arr[i]);
+            } else {
+                int curMax = st.pop();
+                while (!st.empty() && st.peek() > arr[i]) st.pop();
+                st.push(curMax);
+            }
+        }
+        return st.size();
+    }
+}
--- src/main/java/array/simulation/LeetCode_769_MaxChunksToMakeSorted.java
+++ src/main/java/array/simulation/LeetCode_769_MaxChunksToMakeSorted.java
@@ -2,14 +2,33 @@ package array.simulation;
 
 public class LeetCode_769_MaxChunksToMakeSorted {
     public static void main(String[] args) {
-        int list[] = {1,0,2,3,4};//4 [1, 0], [2], [3], [4]
+//        int list[] = {1,0,2,3,4};//4 [1, 0], [2], [3], [4]
+        int list[] = {4,3,2,1,0};//1
         System.out.println(maxChunksToSorted(list));
     }
 
     /**
-     *
+     * 留意 值是下标范围内 可利用
+     * 应该是O(n)
      */
     public static int maxChunksToSorted(int[] arr) {
+        int count = 0, left = 0;
 
+        while(left < arr.length) {
+            if(left == arr[left]) {
+                left++;
+            }
+            else {//因为值是0-arr.length - 1 的一个排列 从小到大遍历 不等于就一定大于
+                int right = arr[left];//搜索范围 在范围内找更大的值
+                while (left <= right){
+                    if(arr[left] > right)
+                        right = arr[left];
+                    left++;
+                }
+            }
+            count++;
+        }
+
+        return count;
     }
 }
