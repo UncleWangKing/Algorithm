d51fac7823c69ac1e04014528ef828957e43fdbb
UncleWangKing <285296372@qq.com>
2018-10-10 03:32:26 +0800

    162

---
+++ src/main/java/array/binarysearch/LeetCode_162_FindPeakElement.java
@@ -0,0 +1,15 @@
+package array.binarysearch;
+
+public class LeetCode_162_FindPeakElement {
+    public static void main(String[] args) {
+        int list[] = {1,2,1,3,5,6,4};
+        System.out.println(findPeakElement(list));
+    }
+
+    /**
+     * 1 2 4地距离扩大 遇到更小值 再4 2 1收缩
+     */
+    public static int findPeakElement(int[] nums) {
+
+    }
+}
