bd114fed477e120ff7ec4110d1c27f2ebd419053
UncleWangKing <285296372@qq.com>
2018-10-15 12:10:49 +0800

    220

--- src/main/java/array/duplicate/LeetCode_220_ContainsDuplicateIII.java
+++ src/main/java/array/duplicate/LeetCode_220_ContainsDuplicateIII.java
@@ -1,14 +1,16 @@
 package array.duplicate;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class LeetCode_220_ContainsDuplicateIII {
     public static void main(String[] args) {
         int list [] = {1,2,3,1};int k = 3;int t = 0;//true
         System.out.println(containsNearbyAlmostDuplicate(list, k, t));
     }
 
+    /**
+     * 和上一题的变化是相等变为了绝对值只差为t
+     * 就不能方便的利用map的key了
+     * k下标差 t值差 都是最大不能超过
+     */
     public static boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
 
     }
