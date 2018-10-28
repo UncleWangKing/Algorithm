190c0d8fbef484ed347fde8fe390b2bfe46fa30d
UncleWangKing <285296372@qq.com>
2018-10-29 02:55:07 +0800

    686

--- src/main/java/str/simulation/LeetCode_686_RepeatedStringMatch.java
+++ src/main/java/str/simulation/LeetCode_686_RepeatedStringMatch.java
@@ -2,10 +2,21 @@ package str.simulation;
 
 public class LeetCode_686_RepeatedStringMatch {
     public static void main(String[] args) {
-        System.out.println(repeatedStringMatch("abcd", "cdabcdab"));
+        System.out.println(repeatedStringMatch("aaaaaaaaaaaaaaaaaaaaaab", "ba"));
     }
 
     public static int repeatedStringMatch(String A, String B) {
-
+        StringBuffer sb = new StringBuffer();
+        sb.append(A);
+        int num = 1;
+        while(sb.length() < B.length()){
+            sb.append(A);
+            num++;
+        }
+        if(-1 != sb.lastIndexOf(B))return num;
+        sb.append(A);
+        num++;
+        if(-1 != sb.lastIndexOf(B))return num;
+        return -1;
     }
 }
