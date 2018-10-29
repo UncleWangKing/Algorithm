5777351c1725f958373ad1e90995ccfa6b9edab7
UncleWangKing <285296372@qq.com>
2018-10-30 04:34:45 +0800

    791

---
+++ src/main/java/str/simulation/LeetCode_791_CustomSortString.java
@@ -0,0 +1,11 @@
+package str.simulation;
+
+public class LeetCode_791_CustomSortString {
+    public static void main(String[] args) {
+        System.out.println(customSortString("cba", "abcd"));//cbad
+    }
+
+    public static String customSortString(String S, String T) {
+
+    }
+}
