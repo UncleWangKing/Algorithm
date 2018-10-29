fded38ac4764e55323067a5ccb1abc01d0be06f7
UncleWangKing <285296372@qq.com>
2018-10-30 04:44:35 +0800

    890 791

--- src/main/java/str/simulation/LeetCode_791_CustomSortString.java
+++ src/main/java/str/simulation/LeetCode_791_CustomSortString.java
@@ -1,11 +1,34 @@
 package str.simulation;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LeetCode_791_CustomSortString {
     public static void main(String[] args) {
         System.out.println(customSortString("cba", "abcd"));//cbad
     }
 
     public static String customSortString(String S, String T) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < T.length(); i++) {
+            map.put(T.charAt(i), map.getOrDefault(T.charAt(i), 0) + 1);
+        }
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < S.length(); i++) {
+            if(map.containsKey(S.charAt(i))) {
+                for (int j = 0; j < map.get(S.charAt(i)); j++) {
+                    builder.append(S.charAt(i));
+                }
+                map.remove(S.charAt(i));
+            }
+        }
 
+        for (Map.Entry<Character, Integer> entry : map.entrySet()){
+            for (int i = 0; i < entry.getValue(); i++) {
+                builder.append(entry.getKey());
+            }
+        }
+
+        return builder.toString();
     }
 }
--- src/main/java/str/simulation/LeetCode_890_FindAndReplacePattern.java
+++ src/main/java/str/simulation/LeetCode_890_FindAndReplacePattern.java
@@ -1,4 +1,42 @@
 package str.simulation;
 
-public class LeetCode_890_FindandReplacePattern {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LeetCode_890_FindAndReplacePattern {
+    public static void main(String[] args) {
+        String[] words  = {"abc","deq","mee","aqq","dkd","ccc"};
+        String pattern = "abb";
+        System.out.println(findAndReplacePattern(words, pattern));
+    }
+
+    public static List<String> findAndReplacePattern(String[] words, String pattern) {
+        List<String> result = new ArrayList<>();
+
+        Map<Character, Character> map = new HashMap<>();
+        Map<Character, Character> reverseMap = new HashMap<>();
+        for (int i = 0; i < words.length; i++) {
+            if(words[i].length() != pattern.length())
+                continue;
+            int j = 0;
+            for (; j < words[i].length(); j++) {
+                if(! map.containsKey(words[i].charAt(j))) {
+                    if(reverseMap.containsKey(pattern.charAt(j)))
+                        break;
+                    map.put(words[i].charAt(j), pattern.charAt(j));
+                    reverseMap.put(pattern.charAt(j), words[i].charAt(j));
+                }
+                else if(map.get(words[i].charAt(j)) != pattern.charAt(j))
+                    break;
+            }
+            if(j == words[i].length())
+                result.add(words[i]);
+            map.clear();
+            reverseMap.clear();
+        }
+
+        return result;
+    }
 }
