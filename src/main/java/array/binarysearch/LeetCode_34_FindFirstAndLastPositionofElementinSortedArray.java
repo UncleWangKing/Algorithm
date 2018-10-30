61a96aeaaf55ffc2b978fac2bb912357a8dfdeae
UncleWangKing <285296372@qq.com>
2018-10-31 04:49:30 +0800

    格式修改 34

--- src/main/java/array/binarysearch/LeetCode_34_FindFirstAndLastPositionofElementinSortedArray.java
+++ src/main/java/array/binarysearch/LeetCode_34_FindFirstAndLastPositionofElementinSortedArray.java
@@ -14,13 +14,11 @@ public class LeetCode_34_FindFirstAndLastPositionofElementinSortedArray {
      * 锻炼二分的写法
      */
     public static int[] searchRange(int[] nums, int target) {
-//        if(nums.length > 0) {
-            int first = findFirst(nums, 0, nums.length - 1, target);
-            if (-1 != first) {
-                int last = findLast(nums, 0, nums.length, target);
-                return new int[]{first, last};
-            }
-//        }
+        int first = findFirst(nums, 0, nums.length - 1, target);
+        if (-1 != first) {
+            int last = findLast(nums, 0, nums.length, target);
+            return new int[]{first, last};
+        }
         return new int[]{-1 ,-1};
     }
 
@@ -39,7 +37,7 @@ public class LeetCode_34_FindFirstAndLastPositionofElementinSortedArray {
         while (begin < end){
             int mid = (begin + end) >>> 1;
             if(nums[mid] <= target)
-                begin = mid +1;
+                begin = mid + 1;
             else
                 end = mid;
         }
