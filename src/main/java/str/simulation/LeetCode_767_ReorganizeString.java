211d5d179d3d2d7553d39132f4f3ea9a03262a07
l05 <285296372@qq.com>
2018-11-02 19:18:19 +0800

    767

---
+++ src/main/java/str/simulation/LeetCode_767_ReorganizeString.java
@@ -0,0 +1,15 @@
+package str.simulation;
+
+/**
+ * @author ZhangDaPang 285296372@qq.com
+ * @date 2018/11/2 19:12
+ */
+public class LeetCode_767_ReorganizeString {
+    public static void main(String[] args) {
+        System.out.println(reorganizeString("aaab"));
+    }
+
+    public static String reorganizeString(String S) {
+
+    }
+}
