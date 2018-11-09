54445e11377eda83be0ade93e251f57d01d462fe
l05 <285296372@qq.com>
2018-11-09 17:24:53 +0800

    583

--- src/main/java/str/subsequence/LeetCode_583_DeleteOperationForTwoStrings.java
+++ src/main/java/str/subsequence/LeetCode_583_DeleteOperationForTwoStrings.java
@@ -5,6 +5,26 @@ public class LeetCode_583_DeleteOperationForTwoStrings {
         System.out.println(minDistance("sea", "eat"));
     }
 
+    /**
+     * 一开始发现LeetCode没有LCS原题我是震精的，直接就是变种题，一点都不美丽。
+     * 求删除最少，那么删除后留下的最长，也就删除最少，也就是求LCS了。
+     * 子序列问题一下扩展到了二维。
+     * 其实就像我们的走楼梯换到了二维一样。
+     * dp[i][j] 代表s1[i - 1]和s2[j - 1]能取到的LCS。
+     * 定义有多种，-1与否是指为了返回或者迭代方便，相信走楼梯过后你已经习惯了。
+     * if(s1[i - 1] == s2[j - 1])
+     * dp[i][j] = dp[i - 1][j - 1] + 1;
+     * else
+     * dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+     * 之前说了，dp找方程是靠状态划分，其实两个字符串的dp，无非就是三种情况。
+     * 1.s1加一个字符
+     * 2.s2加一个字符
+     * 3.s1,s2都加一个字符
+     * 翻译成二维走楼梯对应的就是
+     * 1.和左方有关
+     * 2.和上方有关
+     * 3.和左上方有关
+     */
     public static int minDistance(String word1, String word2) {
         int lcs = longestCS_dp_betterlook(word1, word2);
         return word1.length() + word2.length() - 2 * lcs;
@@ -25,7 +45,7 @@ public class LeetCode_583_DeleteOperationForTwoStrings {
                 if(xList[i - 1] == yList[j - 1])
                     dp[i][j] = dp[i - 1][j - 1] + 1;
                 else
-                    dp[i][j] = Math.max(dp[i - 1][j],dp[i][j - 1]);
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
             }
         }
 
