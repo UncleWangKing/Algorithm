d5ae7b211924b39f1a17c0179e1a9b1462874a7d
l05 <285296372@qq.com>
2018-11-09 18:33:15 +0800

    712

--- src/main/java/str/subsequence/LeetCode_712_MinimumAsciiDeleteSumForTwoStrings.java
+++ src/main/java/str/subsequence/LeetCode_712_MinimumAsciiDeleteSumForTwoStrings.java
@@ -34,8 +34,10 @@ public class LeetCode_712_MinimumAsciiDeleteSumForTwoStrings {
         int[][] dp = new int[m + 1][n + 1];
         for (int i = 1; i <= m; ++i) {
             for (int j = 1; j <= n; ++j) {
-                if (s1.charAt(i - 1) == s2.charAt(j - 1)) dp[i][j] = dp[i - 1][j - 1] + s1.charAt(i - 1);
-                else dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                if (s1.charAt(i - 1) == s2.charAt(j - 1))
+                    dp[i][j] = dp[i - 1][j - 1] + s1.charAt(i - 1);
+                else
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
             }
         }
 
