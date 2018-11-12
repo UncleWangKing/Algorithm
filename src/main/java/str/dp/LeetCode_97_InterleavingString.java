68ce411250cbcdc55544de04559bac2b32b4738e
l05 <285296372@qq.com>
2018-11-12 18:35:12 +0800

    97

--- src/main/java/str/dp/LeetCode_97_InterleavingString.java
+++ src/main/java/str/dp/LeetCode_97_InterleavingString.java
@@ -23,11 +23,10 @@ public class LeetCode_97_InterleavingString {
         }
         for (int i = 1; i <= n1; ++i) {
             for (int j = 1; j <= n2; ++j) {
-                dp[i][j] = (
-                        dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i - 1 + j))
+                dp[i][j] =
+                        (dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i - 1 + j))
                         ||
-                        (dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(j - 1 + i)
-                );
+                        (dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(j - 1 + i));
             }
         }
         return dp[n1][n2];
