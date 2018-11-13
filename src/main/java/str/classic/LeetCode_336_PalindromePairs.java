40ec284e1bc614dc8d514602703d64a32b9c87b9
l05 <285296372@qq.com>
2018-11-13 19:12:39 +0800

    336

---
+++ src/main/java/str/classic/LeetCode_336_PalindromePairs.java
@@ -0,0 +1,18 @@
+package str.classic;
+
+import java.util.List;
+
+/**
+ * @author ZhangDaPang 285296372@qq.com
+ * @date 2018/11/13 19:10
+ */
+public class LeetCode_336_PalindromePairs {
+    public static void main(String[] args) {
+        String []words = {"abcd","dcba","lls","s","sssll"};
+        System.out.println(palindromePairs(words));
+    }
+
+    public static List<List<Integer>> palindromePairs(String[] words) {
+
+    }
+}
