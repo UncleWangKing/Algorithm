b46804b503f7ac8a71d0cf051ec3a267dea99844
UncleWangKing <285296372@qq.com>
2018-11-17 05:57:50 +0800

    10 44

--- src/main/java/str/wildcard/LeetCode_10_RegularExpressionMatching.java
+++ src/main/java/str/wildcard/LeetCode_10_RegularExpressionMatching.java
@@ -13,48 +13,10 @@ public class LeetCode_10_RegularExpressionMatching {
 //        s = "ab"; p = ".*";//true
 //        System.out.println(isMatch(s, p));
 //        s = "aaa"; p = "ab*a*c*a";//true
-        System.out.println(isMatch(s, p));
+        System.out.println(isMatch2(s, p));
     }
 
-    /**
-     * 思路:分类讨论
-     * 1. *.    aaab a*.b     //如何只让*配一个
-     * 2. .*    abcac a.*c   //如何配到第二个c
-     * 3. ** 和 *一个意思
-     * 4. .. 没啥特别两个任意字符而已
-     * 那么关键就在 *和. 的两种组合的情况
-     */
     public static boolean isMatch(String s, String p) {
-        for (int i = 0; i < s.length(); i++) {
-            if(i >= p.length()) {
-                return false;
-            }else if('*' == p.charAt(i)){
-                if(i > 0)
-                    return isWildcardMatch(p.charAt(i - 1), s.substring(i, s.length()), p.substring(i, p.length()));
-                else //p是*开头 直接去了
-                    return isMatch(s.substring(i, s.length()), p.substring(i + 1, p.length()));
-            }else if(s.charAt(i) != p.charAt(i) && '.' != p.charAt(i)){//String s = "aab", p = "c*a*b" c可能是0次 看下后面是否为*
-                if(i + 1 < p.length() && '*' == p.charAt(i + 1))
-                    return isMatch(s.substring(i, s.length()), p.substring(i + 2, p.length()));
-                else
-                    return false;
-            }//'.' == p.charAt(i) 一定能配上 算过 .*情况会被pre拿出来
-        }
-        return s.length() >= p.length();
-    }
-
-    public static boolean isWildcardMatch(char pre, String s, String p) {
-        int index = 0;
-        for (int i = 0; i < s.length() && (pre == s.charAt(i) || pre == '.'); i++) {
-            if(isMatch(s.substring(index, s.length()), p.substring(1, p.length()))){
-                return true;
-            }
-
-        }
-        return false;
-    }
-
-    public static boolean isMatch2(String s, String p) {
         if (p.isEmpty()) return s.isEmpty();
         if (1 == p.length()) {
             return (1 == s.length() && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.'));
@@ -68,5 +30,33 @@ public class LeetCode_10_RegularExpressionMatching {
             s = s.substring(1, s.length());
         }
         return isMatch(s, p.substring(2, p.length()));
+    }
+
+    /**
+     * StackOverflowError
+     */
+    public static boolean isMatch2(String s, String p) {
+        if (p.isEmpty()) return s.isEmpty();
+        if (p.length() > 1 && p.charAt(1) == '*') {
+            return isMatch2(s, p.substring(2, p.length())) || (! s.isEmpty() && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.') && isMatch2(s.substring(1, s.length()), p));
+        } else {
+            return !s.isEmpty() && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.') && isMatch2(s.substring(1, s.length()), p.substring(1, p.length()));
+        }
+    }
+
+    public static boolean isMatch3(String s, String p) {
+        int m = s.length(), n = p.length();
+        boolean dp[][] = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        for (int i = 0; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (j > 1 && p.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i][j - 2] || (i > 0 && (s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.') && dp[i - 1][j]);
+                } else {
+                    dp[i][j] = i > 0 && dp[i - 1][j - 1] && (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.');
+                }
+            }
+        }
+        return dp[m][n];
     }
 }
--- src/main/java/str/wildcard/LeetCode_44_WildcardMatching.java
+++ src/main/java/str/wildcard/LeetCode_44_WildcardMatching.java
@@ -8,6 +8,53 @@ public class LeetCode_44_WildcardMatching {
     }
 
     public static boolean isMatch(String s, String p) {
-        return false;
+        int sIndex = 0, pIndex = 0, match = 0, starIdx = -1;
+        while (sIndex < s.length()){
+            // advancing both pointers
+            if (pIndex < p.length()  && (p.charAt(pIndex) == '?' || s.charAt(sIndex) == p.charAt(pIndex))){
+                sIndex++;
+                pIndex++;
+            }
+            // * found, only advancing pattern pointer
+            else if (pIndex < p.length() && p.charAt(pIndex) == '*'){
+                starIdx = pIndex;
+                match = sIndex;
+                pIndex++;
+            }
+            // last pattern pointer was *, advancing string pointer
+            else if (starIdx != -1){
+                pIndex = starIdx + 1;
+                match++;
+                sIndex = match;
+            }
+            //current pattern pointer is not star, last patter pointer was not *
+            //characters do not match
+            else return false;
+        }
+
+        //check for remaining characters in pattern
+        while (pIndex < p.length() && p.charAt(pIndex) == '*')
+            pIndex++;
+
+        return pIndex == p.length();
+    }
+
+    public static boolean isMatch2(String s, String p) {
+        int m = s.length(), n = p.length();
+        boolean [][]dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        for (int i = 1; i <= n; ++i) {
+            if (p.charAt(i - 1) == '*') dp[0][i] = dp[0][i - 1];
+        }
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (p.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                } else {
+                    dp[i][j] = (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '?') && dp[i - 1][j - 1];
+                }
+            }
+        }
+        return dp[m][n];
     }
 }
