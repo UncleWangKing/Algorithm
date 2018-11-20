42e549ed8c222de7dfcfe6d1b4ad3e381ddaf2f8
UncleWangKing <285296372@qq.com>
2018-11-21 02:08:17 +0800

    567

--- src/main/java/str/slidwindow/LeetCode_567_PermutationInString.java
+++ src/main/java/str/slidwindow/LeetCode_567_PermutationInString.java
@@ -6,10 +6,21 @@ package str.slidwindow;
  */
 public class LeetCode_567_PermutationInString {
     public static void main(String[] args) {
-
+        String s1 = "ab", s2 = "eidbaooo";
+        System.out.println(checkInclusion(s1, s2));
     }
 
     public static boolean checkInclusion(String s1, String s2) {
-        return true;
+        int n1 = s1.length(), n2 = s2.length(), cnt = n1, left = 0;
+        int[] m = new int[128];
+        for (char c : s1.toCharArray()) ++m[c];
+        for (int right = 0; right < n2; ++right) {
+            if (m[s2.charAt(right)]-- > 0) --cnt;
+            while (cnt == 0) {
+                if (right - left + 1 == n1) return true;
+                if (++m[s2.charAt(left++)] > 0) ++cnt;
+            }
+        }
+        return false;
     }
 }
