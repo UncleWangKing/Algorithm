3568fba50e1906947d6e0349065c06be7b230669
UncleWangKing <285296372@qq.com>
2018-12-09 17:17:33 +0800

    630

---
+++ src/main/java/topological/LeetCode_630_CourseScheduleIII.java
@@ -0,0 +1,12 @@
+package topological;
+
+public class LeetCode_630_CourseScheduleIII {
+    public static void main(String[] args) {
+        int [][]list = {{100, 200}, {200, 1300}, {1000, 1250}, {2000, 3200}};
+        System.out.println(scheduleCourse(list));
+    }
+
+    public static int scheduleCourse(int[][] courses) {
+
+    }
+}
