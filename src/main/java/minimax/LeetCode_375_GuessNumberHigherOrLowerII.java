df3bb0d9d3d9db328f9ea90920d423d86bfeaf51
UncleWangKing <285296372@qq.com>
2018-12-11 09:38:04 +0800

    375

--- src/main/java/minimax/LeetCode_375_GuessNumberHigherOrLowerII.java
+++ src/main/java/minimax/LeetCode_375_GuessNumberHigherOrLowerII.java
@@ -11,7 +11,13 @@ public class LeetCode_375_GuessNumberHigherOrLowerII {
             for (int j = i - 1; j > 0; --j) {
                 int global_min = Integer.MAX_VALUE;
                 for (int k = j + 1; k < i; ++k) {
+                    /**
+                     * [j,i]范围内猜值 最坏情况
+                     */
                     int local_max = k + Math.max(dp[j][k - 1], dp[k + 1][i]);
+                    /**
+                     * 所有最坏的情况中 取最小
+                     */
                     global_min = Math.min(global_min, local_max);
                 }
                 dp[j][i] = j + 1 == i ? j : global_min;
