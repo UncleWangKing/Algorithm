6ccb10bfe10227f85aef9f6152429e98a2aa0a02
UncleWangKing <285296372@qq.com>
2019-06-17 17:19:40 +0800

    圣彼得堡悖论游戏

--- src/main/java/funny/SaintPetersburgGame.java
+++ src/main/java/funny/SaintPetersburgGame.java
@@ -2,6 +2,24 @@ package funny;
 
 import java.util.Random;
 
+/**
+ * 圣彼得堡悖论游戏
+ * 玩法:
+ * 抛硬币，直至出现正面(1正面0背面)游戏结束。
+ * 奖励为2的出现正面的次数n的次方  2^n
+ * 例如
+ * 第一次出现正面奖励为2
+ * 第二次才出现正面奖励为4
+ * 依次类推为 8 16 32.....
+ * 纯数学上来说这个游戏的期望收益是无穷大
+ * 期望E = 2^1 * p1 + 2^2 * p2 + 2^3 * p3 + ... + 2^n * pn
+ * p1 p2 p3 分别是
+ * p1 = (1/2)^1
+ * p2 = (1/2)^2
+ * p3 = (1/2)^3
+ * E = 1 + 1 + 1 + .... + 1  = 无穷大
+ * 但是前提是玩无穷多次 期望才是无穷大 这个程序验证了玩大概100万次期望在20左右。
+ */
 public class SaintPetersburgGame {
     public static void main(String[] args) {
         Random random = new Random();
