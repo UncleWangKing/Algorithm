430ba0cfa9df002bffd265ae154b9a8453e6dce5
UncleWangKing <285296372@qq.com>
2019-06-17 17:24:03 +0800

    修改变量名

--- src/main/java/funny/SaintPetersburgGame.java
+++ src/main/java/funny/SaintPetersburgGame.java
@@ -26,21 +26,21 @@ public class SaintPetersburgGame {
         /**
          * 奖金总数
          */
-        long total_sum = 0;
+        long totalReward = 0;
         /**
          * 玩的次数
          */
-        long times = 1000_000;
-        for (int i = 0; i < times; i++) {
+        long count = 1000_000;
+        for (int i = 0; i < count; i++) {
             long reward = 2;
             while (1 != random.nextInt(2))
                 reward *= 2;
-            total_sum += reward;
+            totalReward += reward;
         }
         /**
          * 平均每次收益
          */
-        System.out.println("平均每次收益" + total_sum * 1.0d / times);
+        System.out.println("平均每次收益" + totalReward * 1.0d / count);
     }
 
 
